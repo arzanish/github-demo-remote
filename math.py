ec29eacb5d4470b1cf05fc0636b632739c65e46f
Arzanish Yusuf Nadeem <54699317+arzanish@users.noreply.github.com>
2022-02-02 21:05:50 +0530

    minor updates

--- math.py
+++ math.py
@@ -1,19 +1,12 @@
-# add implimentation
 def add(x,y):
-    return x+y;
-    
-# subtract implimentation
-def subtract(x,y):
-    return x-y;  # on master branch
-    
-# multiply implimentation
-def multiply(x,y):
-    return x*y;  # on bug456    
-    
-# divide implimentation
-def divide(x,y):
-    if y==0: return DIVIDE_BY_ZERO_ERROR  # on Master Branch
-    else : retuen x/y
-    
-def square(x):
     pass
+
+def subtract(x,y):
+    pass 
+
+def multiply(x,y):
+    pass        
+
+def divide(x,y):
+    pass
+    
