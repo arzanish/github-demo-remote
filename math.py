f03662882d74004a80c5fd0dfe6a0db5b626eab3
Arzanish Yusuf Nadeem <54699317+arzanish@users.noreply.github.com>
2022-02-02 21:03:45 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+# add implimentation
+def add(x,y):
+    return x+y;
+    
+# subtract implimentation
+def subtract(x,y):
+    return x-y;  # on master branch
+    
+# multiply implimentation
+def multiply(x,y):
+    return x*y;  # on bug456    
+    
+# divide implimentation
+def divide(x,y):
+    if y==0: return DIVIDE_BY_ZERO_ERROR  # on Master Branch
+    else : retuen x/y
+    
+def square(x):
+    pass
